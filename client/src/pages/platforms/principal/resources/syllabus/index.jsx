9859d486a9d19ec5d29b6220f695e9eda84aaa3c
RDarrel <150007355+magtalas555@users.noreply.github.com>
2023-12-04 12:19:47 +0800

    sylBUS

--- client/src/pages/platforms/principal/resources/syllabus/index.jsx
+++ client/src/pages/platforms/principal/resources/syllabus/index.jsx
@@ -1,53 +1,93 @@
 import React, { useState } from "react";
+import { whiteSpace } from "../../../../../services/utilities";
 import {
   MDBCard,
   MDBCardBody,
-  MDBCardTitle,
   MDBCol,
   MDBCollapse,
   MDBCollapseHeader,
   MDBContainer,
   MDBIcon,
   MDBRow,
+  MDBTable,
+  MDBTableBody,
+  MDBTableHead,
 } from "mdbreact";
-import "./style/card.css";
 
-export default function Syllabus() {
-  const [isOpen, setIsOpen] = useState(false);
+export default function DND() {
+  const [listItems, setListItems] = useState([
+      { id: 1, name: "Item 1" },
+      { id: 2, name: "Item 2" },
+      { id: 3, name: "Item 3" },
+      { id: 4, name: "Item 4" },
+      { id: 5, name: "Item 5" },
+    ]),
+    [isOpen, setIsOpen] = useState(false);
 
   const toggle = () => setIsOpen(!isOpen);
+
+  const [tableItems, setTableItems] = useState([
+    { id: 6, name: "Initial Item" }, // Add an initial item here
+  ]);
+
+  const handleDragStart = (e, item, source) => {
+    e.dataTransfer.setData("text/plain", JSON.stringify({ item, source }));
+  };
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+  };
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+    const data = e.dataTransfer.getData("text/plain");
+    const { item, source } = JSON.parse(data);
+
+    if (source === "list") {
+      // Add the dragged item to the tableItems
+      setTableItems((prevItems) => [...prevItems, item]);
+
+      // Remove the dragged item from listItems
+      setListItems((prevItems) => prevItems.filter((i) => i.id !== item.id));
+    } else if (source === "table") {
+      // Add the dragged item to the listItems
+      setListItems((prevItems) => [...prevItems, item]);
+
+      // Remove the dragged item from tableItems
+      setTableItems((prevItems) => prevItems.filter((i) => i.id !== item.id));
+    }
+  };
+
   return (
     <MDBContainer>
-      <MDBCard>
-        <MDBCardBody>
-          <MDBRow>
-            <MDBCol md="4">
-              <MDBCard>
-                <MDBCardTitle className="ml-3 mt-4">Subjects</MDBCardTitle>
-                <MDBCardBody>
-                  <ul class="list-group list-group-light list-group-small">
-                    <li class="list-group-item">
-                      Oral communication on Context
-                    </li>
-                    <li class="list-group-item">
-                      English Language or Literature
-                    </li>
-                    <li class="list-group-item">
-                      Science (Physics, Chemistry, Biology)
-                    </li>
-                    <li class="list-group-item">
-                      Social Studies or Social Sciences (History, Geography,
-                      Civics)
-                    </li>
-                    <li class="list-group-item">
-                      Foreign Languages (Spanish, French, German, etc.)
-                    </li>
-                  </ul>
-                </MDBCardBody>
-              </MDBCard>
-            </MDBCol>
-            <MDBCol>
-              <MDBCard className="mb-3 shadow-box-example hoverable">
+      <MDBRow>
+        <MDBCol md="4">
+          <MDBCard>
+            <MDBCardBody>
+              <h3>Subjects</h3>
+              <hr />
+              <ul
+                className="list-group ml-3"
+                onDragOver={handleDragOver}
+                onDrop={(e) => handleDrop(e)}
+              >
+                {listItems.map((item) => (
+                  <li
+                    key={item.id}
+                    onDragStart={(e) => handleDragStart(e, item, "list")}
+                    draggable
+                  >
+                    {item.name}
+                  </li>
+                ))}
+              </ul>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+        <MDBCol md="8">
+          <MDBCard>
+            <MDBCardBody>
+              <MDBCard className="mb-3 shadow-box-example">
                 <MDBCollapseHeader className="bg-white" onClick={toggle}>
                   <div className="d-flex justify-content-between align-items-center">
                     <span>Grade 11</span>
@@ -62,134 +102,82 @@ export default function Syllabus() {
                 </MDBCollapseHeader>
                 <MDBCollapse isOpen={isOpen}>
                   <MDBCardBody>
-                    <div style={{ display: "block" }} className="ml-4">
-                      <MDBRow className="mb-2">
-                        <div className="core">CORE SUBJECTS</div>
-                        <MDBCol className="font-weight-bold text-center">
-                          1st Semester
-                        </MDBCol>
-                        <MDBCol className="font-weight-bold text-center">
-                          2nd Semester
-                        </MDBCol>
-                      </MDBRow>
-
-                      <MDBRow>
-                        <MDBCol md="6">
-                          <ul class="list-group list-group-light list-group-small">
-                            <li class="list-group-item">
-                              Oral communication on Context
-                            </li>
-                            <li class="list-group-item">
-                              English Language or Literature
-                            </li>
-                            <li class="list-group-item">
-                              Science (Physics, Chemistry, Biology)
-                            </li>
-                            <li class="list-group-item">
-                              Social Studies or Social Sciences (History,
-                              Geography, Civics)
-                            </li>
-                            <li class="list-group-item">
-                              Foreign Languages (Spanish, French, German, etc.)
-                            </li>
-                          </ul>
-                        </MDBCol>
-                        <MDBCol md="6">
-                          <ul class="list-group list-group-light list-group-small">
-                            <li class="list-group-item">
-                              Oral communication on Context
-                            </li>
-                            <li class="list-group-item">
-                              English Language or Literature
-                            </li>
-                            <li class="list-group-item">
-                              Science (Physics, Chemistry, Biology)
-                            </li>
-                            <li class="list-group-item">
-                              Social Studies or Social Sciences (History,
-                              Geography, Civics)
-                            </li>
-                            <li class="list-group-item">
-                              Foreign Languages (Spanish, French, German, etc.)
-                            </li>
-                          </ul>
-                        </MDBCol>
-                      </MDBRow>
-                    </div>
-                    <div className="ml-4 mt-3">
-                      <MDBRow>
-                        <div className="contextualized">
-                          CONTEXTUALIZED SUBJECT
-                        </div>
-                        <MDBCol>
-                          <ul class="list-group list-group-light list-group-small">
-                            <li class="list-group-item">
-                              empowerment technologies
-                            </li>
-                            <li class="list-group-item"></li>
-                            <li class="list-group-item"></li>
-                          </ul>
-                        </MDBCol>
-                        <MDBCol>
-                          <ul class="list-group list-group-light list-group-small">
-                            <li class="list-group-item">
-                              Oral communication on Context
-                            </li>
-                            <li class="list-group-item"></li>
-                            <li class="list-group-item"></li>
-                          </ul>
-                        </MDBCol>
-                      </MDBRow>
-                    </div>
-                    <div className="ml-4 mt-3">
-                      <MDBRow>
-                        <div className="specialization">
-                          SPECIALIZATION SUBJECT
-                        </div>
-                        <MDBCol>
-                          <ul class="list-group list-group-light list-group-small">
-                            <li class="list-group-item">
-                              empowerment technologies
-                            </li>
-                            <li class="list-group-item"></li>
-                            <li class="list-group-item"></li>
-                            <li class="list-group-item"></li>
-                          </ul>
-                        </MDBCol>
-                        <MDBCol>
-                          <ul class="list-group list-group-light list-group-small">
-                            <li class="list-group-item">
-                              Oral communication on Context
-                            </li>
-                            <li class="list-group-item"></li>
-                            <li class="list-group-item"></li>
-                            <li class="list-group-item"></li>
-                          </ul>
-                        </MDBCol>
-                      </MDBRow>
-                    </div>
-                    <div className="ml-4 mt-3">
-                      <MDBRow>
-                        <div className="hours-per-day">HOURS PER DAY</div>
-                        <MDBCol>
-                          <ul class="list-group list-group-light list-group-small">
-                            <li class="list-group-item">6.6</li>
-                          </ul>
-                        </MDBCol>
-                        <MDBCol>
-                          <ul class="list-group list-group-light list-group-small">
-                            <li class="list-group-item">6.6</li>
-                          </ul>
-                        </MDBCol>
-                      </MDBRow>
-                    </div>
+                    <MDBRow>
+                      <MDBCol md="1">
+                        <span style={{ whiteSpace: "pre" }}>
+                          {whiteSpace("CORE SUBJECTS")}
+                        </span>
+                      </MDBCol>
+                      <MDBCol md="11">
+                        <MDBTable
+                          onDragOver={handleDragOver}
+                          onDrop={(e) => handleDrop(e)}
+                          className="table table-bordered"
+                        >
+                          <MDBTableHead>
+                            <tr>
+                              <th className="text-center font-weight-bold border-right">
+                                1st Semester
+                              </th>
+                              <th className="text-center font-weight-bold">
+                                2nd Semester
+                              </th>
+                            </tr>
+                          </MDBTableHead>
+                          <MDBTableBody>
+                            {tableItems.map((item) => (
+                              <tr
+                                key={item.id}
+                                onDragStart={(e) =>
+                                  handleDragStart(e, item, "table")
+                                }
+                                draggable
+                              >
+                                <td>{item.id}</td>
+                                <td>{item.name}</td>
+                                <td></td>
+                              </tr>
+                            ))}
+                          </MDBTableBody>
+                        </MDBTable>
+                      </MDBCol>
+                    </MDBRow>
                   </MDBCardBody>
                 </MDBCollapse>
               </MDBCard>
-            </MDBCol>
-          </MDBRow>
-        </MDBCardBody>
-      </MDBCard>
+              {/* <table
+                className="table"
+                onDragOver={handleDragOver}
+                onDrop={(e) => handleDrop(e)}
+              >
+                <thead>
+                  <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td style={{ whiteSpace: "pre" }}>
+                      {whiteSpace("CORE SUBJECTS")}
+                    </td>
+                  </tr>
+                  {tableItems.map((item) => (
+                    <tr
+                      key={item.id}
+                      onDragStart={(e) => handleDragStart(e, item, "table")}
+                      draggable
+                    >
+                      <td>{item.id}</td>
+                      <td>{item.name}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table> */}
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+      </MDBRow>
     </MDBContainer>
   );
 }
