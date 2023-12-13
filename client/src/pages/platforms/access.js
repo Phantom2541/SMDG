c7010edee74b4316c12264b5b9bc03729bcf5645
Phantom2541 <78855038+Phantom2541@users.noreply.github.com>
2023-12-13 21:25:22 +0800

    update all

--- client/src/pages/platforms/access.js
+++ client/src/pages/platforms/access.js
@@ -1,24 +1,16 @@
 import ADMINISTRATOR from "./administrator/access";
 import PRINCIPAL from "./principal/access";
-import VICE from "./vice/access";
 import HEAD from "./head/access";
 import MASTER from "./master/access";
 import TEACHER from "./teacher/access";
-import REGISTRAR from "./registrar/access";
-import CASHIER from "./cashier/access";
-import GUIDANCE from "./guidance/access";
 import OTHERS from "./others/access";
 import STUDENT from "./student/access";
 
 const ACCESS = {
   ADMINISTRATOR,
   PRINCIPAL,
-  VICE,
   MASTER,
   TEACHER,
-  REGISTRAR,
-  CASHIER,
-  GUIDANCE,
   OTHERS,
   HEAD,
   STUDENT,
--- client/src/services/fakeDb/index.js
+++ client/src/services/fakeDb/index.js
@@ -1,8 +1,7 @@
-import QuestionTypes from "./questionTypes";
 import Philippines from "./philippines";
 import Roles from "./roles";
 import Departments from "./departments";
 import School from "./school";
 import Courses from "./courses";
 
-export { QuestionTypes, Philippines, Roles, Departments, School, Courses };
+export { Philippines, Roles, Departments, School, Courses };
